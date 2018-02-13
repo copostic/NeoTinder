cb10d93fff740e7ada061535355bc6ea30f85215
Corentin <corentin@yachtsupply.io>
2018-02-13 16:22:10 +0100

    Test version of fb app

--- config/auth.js
+++ config/auth.js
@@ -1,8 +1,8 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'      : '145793879439435',
-        'clientSecret'  : '499f9ad871bbd03d66828cb2b01e9f58',
+        'clientID'      : '320551335017584',
+        'clientSecret'  : 'b8b7475a5fc4a10dcb2f34dfe92d04bf',
         'callbackURL'   : 'http://tineo.ga/auth/facebook/callback',
         'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'profileFields' : ['id', 'name','emails', 'picture.type(large)', 'about', 'gender', 'birthday', 'interested_in'],
