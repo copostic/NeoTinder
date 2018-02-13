46a0fe356a6fc81d271cd95363b394c18f5f01f9
Corentin <corentin@yachtsupply.io>
2018-02-13 15:31:19 +0100

    hotfix

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    'url' : 'mongodb://localhost/tineo' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://localhost/Tineo' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
