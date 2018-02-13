225dc647ee91108fd9aebc5f6e558a40782062aa
Corentin <corentin@yachtsupply.io>
2018-02-13 16:16:09 +0100

    enableproof

--- config/auth.js
+++ config/auth.js
@@ -5,7 +5,8 @@ module.exports = {
         'clientSecret'  : '499f9ad871bbd03d66828cb2b01e9f58',
         'callbackURL'   : 'http://tineo.ga/auth/facebook/callback',
         'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
-        'profileFields' : ['id', 'name','emails', 'picture.type(large)', 'about', 'gender', 'birthday', 'interested_in']
+        'profileFields' : ['id', 'name','emails', 'picture.type(large)', 'about', 'gender', 'birthday', 'interested_in'],
+        'enableProof'   : 'true'
     }
 
 };
